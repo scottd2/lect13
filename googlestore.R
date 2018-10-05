fcc828667d1aa0e102284ae46c641f139d89e2cc
scottd2 <scottd2.bu.edu>
2018-10-05 16:01:29 -0400

    added reviews by installs

---
+++ googlestore.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+
+gstore <- read_csv("data/googleplaystore.csv")
+
+gstore %>% group_by(App) %>%
+  summarize(Reviews=mean(Reviews), Rating=mean(Rating)) %>%
+  arrange(desc(Reviews)) %>% select(App, Reviews,Rating)
+
+
+
+#data cleaning
+gstore<-gstore %>% 
+  filter(!is.na(Category),Type !="NaN",!is.nan(Rating))
+
+
+
+
+#reviews by installs
+ggplot(gstore) + geom_boxplot(aes(x=Installs,y=Reviews))+
+  coord_flip()
+
+#boxplot grouped
+gstore %>% group_by(App,Installs) %>% summarize(Reviews=mean(Reviews)) %>%
+  ggplot+geom_boxplot(aes(Installs,Reviews))+coord_flip()
+
+#notice the scales are not equal
+gstore %>% group_by(App,Installs) %>% summarize(Reviews=mean(Reviews)) %>%
+  ggplot+geom_boxplot(aes(Installs,log(Reviews)))+coord_flip()
+
+#now we need to order
+gstore %>% group_by(App,Installs) %>% summarize(Reviews=mean(Reviews)) %>%
+  ggplot+geom_boxplot(aes(reorder(Installs,Reviews,median),log(Reviews)))+
+  coord_flip()
+
+
+
+
+                      
